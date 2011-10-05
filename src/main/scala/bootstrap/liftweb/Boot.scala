8ad07afc4da7853526e40cbb0484f99c1641e566
Johan Prinsloo <johan.a.prinsloo@gmail.com>
2011-10-05 09:00:08 -0700

    xml rest http auth play

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -8,12 +8,18 @@ import http._
 import sitemap._
 import Loc._
 import code.rest._
+import net.liftweb.http.auth.HttpDigestAuthentication
+import net.liftweb.http.auth.userRoles
+import net.liftweb.http.auth.AuthRole
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
 class Boot {
+  
+  val logger = Logger(classOf[Boot])
+  
   def boot {
     // where to search snippet
     LiftRules.addToPackages("code")
@@ -53,6 +59,29 @@ class Boot {
     
     LiftRules.dispatch.append(XmlRest.dispatch)
     LiftRules.statelessDispatchTable.append(XmlRest.dispatch) // stateless -- no session created
-    
+     LiftRules.httpAuthProtectedResource.append(XmlRest.protection)
+
+     LiftRules.authentication = HttpDigestAuthentication("realm") {
+      case (username, req, authfunc) => {
+        val pw = "pw" //fake lookup username -> pw
+        logger.info("Digest auth request with " + username + " :lookup pw :" + pw + ": . " + req.toString() )
+        if ( authfunc(pw) ) {
+        	userRoles(List(AuthRole("authenticated")))
+            logger.info(" user authenticated with digest " )
+            true
+        }
+        else
+        {
+          logger.info(" user not authenticated with digest")
+          false
+        }
+      }
+      case _ => {
+        logger.debug(" bad client call for digest authentication")
+        false
+      }
+    }
+
   }
+
 }
--- src/main/scala/code/rest/XmlRest.scala
+++ src/main/scala/code/rest/XmlRest.scala
@@ -14,6 +14,8 @@ import net.liftweb.http.auth.AuthRole
 import net.liftweb.json._
 import net.liftweb.http.JsonResponse
 import net.liftweb.http.XmlResponse
+import net.liftweb.common.EmptyBox
+import net.liftweb.common.Empty
 
 object XmlRest extends XMLApiHelper with Logger {
 
@@ -24,16 +26,20 @@ object XmlRest extends XMLApiHelper with Logger {
 
   def dispatch: LiftRules.DispatchPF = {
 
-    case Req(List("api", "q1"), _, GetRequest)         =>  { () => JsonResponse( JString(" api => q1" )) }
-    case Req(List("api", "q1", id), _, GetRequest) =>  { () => XmlResponse( <b> api q1 {id}</b>) }
-    case Req(List("api", "q1", id , "cond1" ), _, GetRequest) =>  { () => XmlResponse( <b> api q1 {id} cond1</b>) }
-    case Req(List("api", "q1", "q2", "cond2" ), _, GetRequest) =>  { () => XmlResponse( <b> api q1 q2 cond2</b>) }
-    
+    case Req(List("api", "q1"), _, GetRequest) => { () => JsonResponse(JString(" api => q1")) }
+    case Req(List("api", "q1", id), _, GetRequest) => { () => XmlResponse(<b> api q1 { id }</b>) }
+    case Req(List("api", "q1", id, "cond1"), _, GetRequest) => { () => XmlResponse(<b> api q1 { id } cond1</b>) }
+    case Req(List("api", "q1", id, "cond2"), _, GetRequest) => { () => XmlResponse(<b> api q1 { id } cond1</b>) }
+
   }
 
   def protection: LiftRules.HttpAuthProtectedResourcePF = {
-    case Req(List("api", "q1", id, whatever, _), _, GetRequest) => {
-      Full(AuthRole("authenticated"))
+    case Req(List("api", "q1", id, something), _, GetRequest) => {
+      info("authentication match on: " + something)
+      something match {
+        case "cond2" => Empty
+        case _ => Full(AuthRole("authenticated"))
+      }
     }
   }
 
